4a777383da934b3c7d0266e4dcb2370695909404
jaeh <jascha@jaeh.at>
2021-09-15 06:00:46 +0200

    add src dir

---
+++ src/artifact.js
@@ -0,0 +1,1 @@
+var a={name:"Artifact",slug:"artifact",sky:"skybox",file:"artifact",cam:{y:2,z:8},lookat:{y:2}};export{a as default};
---
+++ src/render.js
@@ -0,0 +1,1 @@
+var e=(new Date).getTime(),t=(t,a,o)=>{((t,a,o)=>{var r=(new Date).getTime();r>e+1e3&&(e=r,console.log({timestamp:t,frame:a,three:o}))})(t,a,o);var{model:r}=o;r.rotation.x+=.02,r.rotation.y+=.01,r.rotation.z+=.03};export{t as default};
