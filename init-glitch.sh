ebd1a23fe8690d142885255d188bb01a79605987
jaeh <jascha@jaeh.at>
2023-03-15 15:16:12 +0100

    init-glitch: nicer formatting, comments

--- init-glitch.sh
+++ init-glitch.sh
@@ -1,5 +1,21 @@
-rm -rf node_modules package.json package-lock.json shrinkwrap.yaml server.py \
-&& mv src/* . \
-&& rm -rf src \
-&& rm init-glitch.sh \
-&& refresh
+# remove unneeded nodejs and python files
+rm -rf \
+  node_modules \
+  package.json \
+  package-lock.json \
+  shrinkwrap.yaml \
+  server.py \
+  .gitignore \
+  README.md
+
+# move the actual sandbox contents into the main directory
+mv src/* .
+
+# remove the src directory, it is now empty.
+rm -rf src
+
+# remove this file
+rm init-glitch.sh \
+
+# refresh glitch to get the new file hierarchy shown
+refresh
